63aed564c94c53925026bdd6daaaf50c1fc4dc56
Andrew Bosley <abosley@outlook.com>
2024-06-15 16:51:43 +0100

    Starter error and circle

---
+++ src/lib/swirlCircle.svelte
@@ -0,0 +1,62 @@
+<!--
+	@component
+	Circle with orange rotating glow.
+-->
+
+<script lang="ts">
+</script>
+
+<div class="circle">
+	<span></span>
+</div>
+<div class="content">
+	<slot></slot>
+</div>
+
+<style>
+	.content {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		transform-origin: center;
+	}
+
+	.circle {
+		width: 100%;
+		height: 100%;
+		border-radius: 50%;
+		aspect-ratio: 1 / 1;
+		animation: animate 3s linear infinite;
+	}
+
+	@keyframes animate {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+	.circle span {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		border-radius: 50%;
+		background: linear-gradient(transparent, var(--Accent));
+		filter: blur(15px);
+	}
+
+	.circle:after {
+		content: '';
+		position: absolute;
+		top: 20px;
+		left: 20px;
+		right: 20px;
+		bottom: 20px;
+		background: var(--PColor2);
+		border: solid var(--PColor1) 2px;
+		border-radius: 50%;
+	}
+</style>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,171 @@
+<script lang="ts">
+	import SwirlCircle from '$lib/swirlCircle.svelte';
+	import { page } from '$app/stores';
+	import ThemeToggle from '$lib/toggleTheme.svelte';
+	// Change number of BG status codes based on screen width.
+	// Get screen width
+	function getScreenWidth(): number {
+		return window.innerWidth;
+	}
+	let screenSizeVariable: number = getScreenWidth();
+
+	// Change number of codes based on that width
+	function updateVariableBasedOnScreenSize(): void {
+		const screenWidth = getScreenWidth();
+		if (screenWidth < 500) {
+			screenSizeVariable = 20;
+		} else if (screenWidth < 1200) {
+			screenSizeVariable = 60;
+		} else {
+			screenSizeVariable = 130;
+		}
+	}
+	updateVariableBasedOnScreenSize();
+
+	// Place codes over screen at random
+	let bgerrors = [];
+	for (let i = 0; i < screenSizeVariable; i++) {
+		const randomTop = `${Math.floor(Math.random() * 100)}vh`;
+		const randomLeft = `${Math.floor(Math.random() * 100)}vw`;
+		const randomId = Math.floor(Math.random() * 3) + 1;
+
+		bgerrors.push({
+			id: randomId,
+			top: randomTop,
+			left: randomLeft
+		});
+	}
+</script>
+
+<div class="container">
+	{#each bgerrors as bgerror}
+		<div class="random-div" style="top: {bgerror.top}; left: {bgerror.left};" data-id={bgerror.id}>
+			{$page.status}
+		</div>
+	{/each}
+
+	<div class="themeToggle">
+		<ThemeToggle />
+	</div>
+
+	<a href="/">
+		<div class="circle">
+			<SwirlCircle>
+				<h1>{$page.status}</h1>
+				<h2>{$page?.error?.message}</h2>
+			</SwirlCircle>
+		</div>
+	</a>
+</div>
+
+<style>
+	.themeToggle {
+		padding: 10px;
+		position: absolute;
+		top: 0;
+		right: 0;
+		z-index: 10;
+	}
+
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	.circle {
+		animation: blur 20s ease-in-out infinite;
+		width: 500px;
+		height: 500px;
+	}
+
+	h1 {
+		text-align: center;
+		color: var(--TColor);
+		font-weight: bold;
+		font-size: 8em;
+		text-shadow: 0px 10px 0 var(--SColor1);
+	}
+
+	h2 {
+		text-align: center;
+		color: var(--Accent);
+		font-size: 2em;
+		font-weight: bold;
+		text-transform: uppercase;
+	}
+
+	@media (max-width: 1000px) {
+		.circle {
+			width: 300px;
+			height: 300px;
+		}
+
+		h1 {
+			font-size: 4em;
+			text-shadow: 0px -5px 0 var(--SColor1);
+		}
+
+		h2 {
+			font-size: 1em;
+		}
+	}
+
+	.random-div:hover {
+		transform: scale(5);
+		color: var(--Accent);
+		font-weight: bold;
+	}
+
+	.random-div {
+		position: fixed;
+		display: block;
+		animation: fading 3s infinite;
+	}
+
+	.random-div[data-id='1'] {
+		animation-delay: 1s;
+	}
+
+	.random-div[data-id='2'] {
+		animation-delay: 2s;
+	}
+
+	.random-div[data-id='3'] {
+		animation-delay: 3s;
+	}
+
+	@keyframes fading {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+
+	@keyframes blur {
+		0%,
+		45% {
+			filter: blur(0px);
+		}
+
+		49% {
+			filter: blur(5px);
+		}
+
+		50% {
+			filter: blur(0px);
+		}
+
+		51% {
+			filter: blur(5px);
+		}
+
+		55%,
+		100% {
+			filter: blur(0px);
+		}
+	}
+</style>
