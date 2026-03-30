a0aaf4ca93451e75158444e713fb3b98ce38c04d
Andrew Bosley <abosley@outlook.com>
2026-03-30 20:15:16 +0100

    Updates

--- _app/immutable/chunks/BFtx5_rv.js
+++
@@ -1,1 +0,0 @@
-import{b as p,E as t}from"./BMb0_K_S.js";import{B as c}from"./D90nwUfK.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
---
+++ _app/immutable/chunks/DSgMhSWe.js
@@ -0,0 +1,43 @@
+const n=`<?xml version="1.0" encoding="UTF-8" standalone="no"?>\r
+<svg\r
+   role="img"\r
+   width="100%"\r
+   height="100%"\r
+   viewBox="0 0 1024 1024"\r
+   fill="currentColor"\r
+   version="1.1"\r
+   id="svg1"\r
+   sodipodi:docname="bluesky.svg"\r
+   inkscape:version="1.4.3 (0d15f75, 2025-12-25)"\r
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"\r
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"\r
+   xmlns="http://www.w3.org/2000/svg"\r
+   xmlns:svg="http://www.w3.org/2000/svg">\r
+  <defs\r
+     id="defs1" />\r
+  <sodipodi:namedview\r
+     id="namedview1"\r
+     pagecolor="#505050"\r
+     bordercolor="#eeeeee"\r
+     borderopacity="1"\r
+     inkscape:showpageshadow="0"\r
+     inkscape:pageopacity="0"\r
+     inkscape:pagecheckerboard="0"\r
+     inkscape:deskcolor="#505050"\r
+     inkscape:zoom="1.2958984"\r
+     inkscape:cx="512"\r
+     inkscape:cy="512"\r
+     inkscape:window-width="3840"\r
+     inkscape:window-height="1529"\r
+     inkscape:window-x="-8"\r
+     inkscape:window-y="-8"\r
+     inkscape:window-maximized="1"\r
+     inkscape:current-layer="svg1" />\r
+  <title\r
+     id="title1">Bluesky</title>\r
+  <path\r
+     d="m 222.2446,103.76382 c 117.53186,92.3089 243.94359,279.48137 290.36253,379.91694 46.41896,-100.43557 172.83068,-287.60804 290.36253,-379.91694 84.80451,-66.607112 222.21494,-118.142843 222.21494,45.84856 0,32.7522 -17.9491,275.13102 -28.47689,314.48428 C 960.11438,600.90029 826.77202,635.79368 708.15726,614.67449 915.48944,651.59013 968.23078,773.86806 854.32487,896.14599 637.9955,1128.3759 543.39969,837.87892 519.16055,763.44346 c -4.44356,-13.6458 -6.52273,-20.0297 -6.55342,-14.60122 -0.0306,-5.42848 -2.10987,0.95542 -6.55341,14.60122 C 481.81458,837.87892 387.21877,1128.3759 170.88939,896.14599 56.98347,773.86806 109.72482,651.59013 317.05701,614.67449 198.44225,635.79368 65.099887,600.90029 28.506548,464.09666 17.978849,424.7434 0.02961524,182.36458 0.02961524,149.61238 0.02961524,-14.379023 137.44011,37.156708 222.2446,103.76382 Z"\r
+     id="path1"\r
+     style="stroke-width:1.84603" />\r
+</svg>\r
+`;export{n as default};
